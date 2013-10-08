348c1b4bbfb998503649229135f7c040c498d3f0
valentin <valentin@192.168.1.11>
2013-10-08 21:42:56 +0200

    added basic Entity and MovingEntity

--- EpicGreenCube/src/game/Entity.java
+++ EpicGreenCube/src/game/Entity.java
@@ -1,5 +1,100 @@
 package game;
 
-public class Entity {
+import java.awt.image.BufferedImage;
 
+/**
+ * the most basic part of any entity of the game
+ * @author valentin
+ * @version 1
+ */
+public abstract class Entity {
+	
+	//fields
+	
+	// position
+	protected double posX;
+	protected double posY;
+	
+	// spawn position
+	// we use these fields when the player die. It's better than reloading all the level
+	private final double spawnX;
+	private final double spawnY;
+	
+	// Sprite
+	protected BufferedImage sprite;
+	
+	/**
+	 * The constructor for the abstract class Entity
+	 * @param spawnX the position in the X-axis for the entity (used as spawn)
+	 * @param spawnY the position in the Y-axis for the entity (used as spawn)
+	 * @param sprite the BufferedImage used to display this Entity
+	 * 
+	 * TODO : check if the position are in the screen/level ?
+	 */
+	public Entity( double spawnX, double spawnY, BufferedImage sprite) {
+		// we define the spawn and the first position (that is the spawn)
+		this.spawnX = spawnX;
+		this.spawnY = spawnY;
+		this.posX = spawnX;
+		this.posY = spawnY;
+		
+		// and we also define the sprite
+		this.sprite = sprite;
+	}
+	
+	
+	/**
+	 * place the Entity at its spawn
+	 */
+	public void reset(){
+		this.posX = this.spawnX;
+		this.posY = this.spawnY;
+	}
+	
+	/**
+	 * ABSTRACT - update the Entity
+	 */
+	public abstract void update();
+	
+	/**
+	 * get the X-axis position of the Entity
+	 * @return the X-axis position
+	 */
+	public double getPosX() {
+		return this.posX;
+	}
+
+	/**
+	 * get the X-axis position of the Entity
+	 * @return the Y-axis position
+	 */
+	public double getPosY() {
+		return this.posY;
+	}
+
+	/**
+	 * get the sprite of the Entity
+	 * @return the associated sprite (a BufferedImage)
+	 */
+	public BufferedImage getSprite() {
+		return this.sprite;
+	}
+	
+	/**
+	 * get the height of the sprite associated to this Entity
+	 * @return the height of the sprite associated
+	 */
+	public int getHeight() {
+		return this.sprite.getHeight();
+	}
+	
+	/**
+	 * get the width of the sprite associated to this Entity
+	 * @return the width of the sprite associated
+	 */
+	public int getWidth() {
+		return this.sprite.getWidth();
+	}
+	
+	
 }
---
+++ EpicGreenCube/src/game/MovingEntity.java
@@ -0,0 +1,61 @@
+package game;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * an ameliorated Entity that can move
+ * @author valentin
+ * @version 1
+ */
+public abstract class MovingEntity extends Entity {
+
+	//fields
+	private double speedX; // the entity's speed on the X-axis
+	private double speedY; // the entity's speed on the Y-axis
+	
+	
+	/**
+	 * The constructor for the abstract class MovingEntity
+	 * @param spawnX the position in the X-axis for the entity (used as spawn)
+	 * @param spawnY the position in the Y-axis for the entity (used as spawn)
+	 * @param sprite the BufferedImage used to display this Entity
+	 */
+	public MovingEntity(double spawnX, double spawnY, BufferedImage sprite) {
+		super(spawnX, spawnY, sprite);
+	}
+	
+	/**
+	 * set the X speed of the MovingEntity
+	 * @param newSpeed the new speed on the X-axis
+	 */
+	public void setSpeedX(double newSpeed) {
+		this.speedX = newSpeed;
+		this.get
+	}
+	
+	/**
+	 * set the Y speed of the MovingEntity
+	 * @param newSpeed the new speed on the Y-axis
+	 */
+	public void setSpeedY(double newSpeed) {
+		this.speedY = newSpeed;
+	}
+	
+	/**
+	 * get the X speed of the MovingEntity
+	 * @return the speed on the X-axis
+	 */
+	public double getSpeedX() {
+		return this.speedX;
+	}
+
+	/**
+	 * get the Y speed of the MovingEntity
+	 * @return the speed on the Y-axis
+	 */
+	public double getSpeedY() {
+		return this.speedY;
+	}
+
+
+}
