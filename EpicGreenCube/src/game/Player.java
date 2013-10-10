f0302100e8b9d332788922a885dbbe9fe06a66ca
valentin <valentin@valentin-PC.home>
2013-10-10 22:17:05 +0200

    added basic input check

--- EpicGreenCube/src/game/Player.java
+++ EpicGreenCube/src/game/Player.java
@@ -1,7 +1,9 @@
 package game;
 
 import gui.GameSprites;
+import handlers.KeyboardInputHandler;
 
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
 /**
@@ -11,18 +13,40 @@ import java.awt.image.BufferedImage;
  */
 public class Player extends MovingEntity {
 	
-	public final double SPEED = 2.;
+	public final static double SPEED = 2.;
 
 	public Player(double spawnX, double spawnY) {
 		super(spawnX, spawnY, GameSprites.playerSprite());
 		// TODO Auto-generated constructor stub
 	}
 	
-
+	/**
+	 * check the keyboard to adjust speed
+	 */
+	public void checkKeyboard() {
+		// we check if one or more keys are pressed
+		if (KeyboardInputHandler.keys[KeyEvent.VK_UP]) {
+			// To the top !
+			this.speedY = - Player.SPEED;
+		} else if (KeyboardInputHandler.keys[KeyEvent.VK_DOWN]) {
+			// nah, go to the bottom
+			this.speedY =Player.SPEED;
+		}
+		
+		// now we check right and left
+		if (KeyboardInputHandler.keys[KeyEvent.VK_LEFT]) {
+			// always the left first !
+			this.speedX = - Player.SPEED;
+		} else if (KeyboardInputHandler.keys[KeyEvent.VK_RIGHT]) {
+			// and now the right
+			this.speedX =Player.SPEED;
+		}
+	}
 
 	@Override
 	public void update(Updater u) {
-		// TODO : what to do ?
+		// First of all, we check the keyboard to change the speeds
+		checkKeyboard();
 
 		// we update position
 		this.posX += this.speedX;
