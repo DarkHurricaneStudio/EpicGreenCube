5250c496b93d1ff45dc5c0cbc841579da0ad3934
valentin <valentin@valentin-PC.home>
2013-10-12 11:25:20 +0200

    added multiple loading in 4FL (FFL) Maybe we should rename it to ZFL (Zip File Level ?) working on the loading of levels

--- EpicGreenCube/src/FourFileLevel/FFL_Loader.java
+++ EpicGreenCube/src/FourFileLevel/FFL_Loader.java
@@ -15,7 +15,7 @@ import javax.imageio.ImageIO;
 /**
  * this class is used to load files from a Zip archive, for The 4 File Level system 
  * @author Avignak
- * @version 1
+ * @version 2 : added multiple images and texts loading
  */
 public class FFL_Loader {
 	
@@ -77,11 +77,71 @@ public class FFL_Loader {
 	}
 	
 	/**
+	 * Load more than one images from a zip archive
+	 * @param path The path to the zip file (ex: "C:/level.zip" )
+	 * @param filenames The names of the wanted images in an ArrayList
+	 * @return A BufferedImage of the wanted image
+	 */
+	public static ArrayList<BufferedImage> loadMultipleImages(String path, ArrayList<String>filenames) {
+		
+		// the return
+		ArrayList<BufferedImage> images = new ArrayList<BufferedImage>();
+		// we try
+		try {
+			// we create an object to load the file into a byte stream
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+			// we load the Zip file
+			ZipFile zipFile = new ZipFile(path);
+
+
+			// we work on each filename
+			for (int i=0; i < filenames.size();i++) {
+				try {
+					// we search the good file from the zip
+					ZipEntry zipFileEntry = zipFile.getEntry(filenames.get(i));
+
+					// we load the file into an Input Stream
+					InputStream inputStream = zipFile.getInputStream(zipFileEntry);
+
+					// The file will be read at a byte[]
+					byte[] buffer = new byte[FFL_Loader.BUFFER];
+
+					// we load all characters
+					for(int n; (n = inputStream.read(buffer)) != -1; )  
+						out.write(buffer, 0, n);
+
+					// we close the stream
+					inputStream.close();
+
+					// and the Byte[] output stream
+					out.close(); 
+					byte[] bytes = out.toByteArray();
+
+					// we put the image in the ArrayList
+					images.add(ImageIO.read(new ByteArrayInputStream(bytes)));
+				} catch (Exception e) {
+					System.out.println("Cannot load "+filenames.get(i)+" in "+path+" !");
+				}
+			}
+
+			// we close the zip
+			zipFile.close();
+		} catch (Exception e) {
+			System.out.println("Cannot open "+path+" !");
+		}
+
+    	
+    	// finally, we return the BufferedImage or null if the loading doesn't work
+    	return images;
+	}
+	
+	/**
 	 *  Load a text file from a zip archive
 	 * @param path The path to the zip file (ex: "C:/level.zip" )
 	 * @param filename The name of the wanted text file
 	 * @param encoding The encoding system from this file (ex : "UTF-8" )
-	 * @return a vector of String, each entry represents one line from the file
+	 * @return an ArrayList of String, each entry represents one line from the file
 	 */
 	public static ArrayList<String> loadText(String path, String filename, String encoding) {
 		
@@ -127,6 +187,64 @@ public class FFL_Loader {
 	 */
 	public static ArrayList<String> loadText(String path, String filename) {
 		return FFL_Loader.loadText(path, filename, "UTF-8");
+	}
+	
+	/**
+	 *  Load more than one text files from a zip archive
+	 * @param path The path to the zip file (ex: "C:/level.zip" )
+	 * @param filenames The names of the wanted text files (an ArrayList)
+	 * @param encoding The encoding system from this file (ex : "UTF-8" )
+	 * @return an array of ArrayList of String, each entry represents one line from the file
+	 */
+	public static ArrayList<ArrayList<String>> loadMultipleTexts(String path, ArrayList<String> filenames, String encoding) {
+
+		// we create the output
+		ArrayList<ArrayList<String>> files = new ArrayList<ArrayList<String>>();
+
+		// we try to open the zip file
+		try {
+			// we load the Zip file
+			ZipFile zipFile = new ZipFile(path);
+
+			// we search for all filenames
+			for (int i = 0; i < filenames.size();i++) {
+				try {
+					// we search the good file from the zip
+					ZipEntry zipFileEntry = zipFile.getEntry(filenames.get(i));
+
+					// we load it as an Input Stream
+					InputStream inputStream = zipFile.getInputStream(zipFileEntry);
+
+					// we create a line in files
+					files.add(i,new ArrayList<String>());
+
+					// we load the stream in a bufferedReader
+					BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, encoding));
+
+					// we read each line one by one and stock it in the vector
+					String line = null;
+					while ((line = bufferedReader.readLine()) != null)  {
+						files.get(i).add(line);
+					}
+
+
+					// we close the Input stream
+					inputStream.close();
+				} catch (Exception e) {
+					System.out.println("Cannot load "+filenames.get(i)+" in "+path+" !");
+				}
+
+			}
+
+			// same thing for the zip
+			zipFile.close();  
+
+		} catch (Exception e) {
+			System.out.println("Cannot open "+path+" !");
+		}
+
+
+		return files;
 	}
 	
 }
--- EpicGreenCube/src/game/Level.java
+++ EpicGreenCube/src/game/Level.java
@@ -3,6 +3,8 @@ package game;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
+import FourFileLevel.FFL_Loader;
+
 /**
  * The level class, representing... a level
  * @author valentin
@@ -14,5 +16,29 @@ public class Level {
 	private ArrayList<MovingEntity> enemies; // all enemies : circles and triangles
 	private BufferedImage background; // the background of the level
 	private ArrayList<String> messages; // some messages to display ?
+	
+	/**
+	 * Load and create a Level from a ZIP with only a number as param
+	 * @param zipName the name of the ZIP file
+	 * @param id the ID of the level
+	 */
+	public Level(BufferedImage image, ArrayList<String> textFile) {
+		// we load the image representing the level
+		this.background = image;
+		// and we do the same for the text file
+		loadTextLevel(textFile);
+		
+	}
+	
+	/**
+	 * Transform the text file into usable content
+	 * @param textFile an arrayList of string representing the file
+	 */
+	private void loadTextLevel(ArrayList<String> textFile) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
 
 }
