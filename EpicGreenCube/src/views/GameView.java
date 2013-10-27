89a6ab3555cb6c92f57564a94914d7bb6d08b335
Robrock <rob.keb@hotmail.fr>
2013-10-27 21:46:29 +0100

    Added main game loop

--- EpicGreenCube/src/views/GameView.java
+++ EpicGreenCube/src/views/GameView.java
@@ -18,6 +18,32 @@ public class GameView extends View {
 	 */
 	private Updater updater;
 
+	
+	/**
+	 * 
+	 */
+	private long beforeTime, timeDiff, afterTime, sleepTime,overSleepTime;
+	
+	/**
+	 * Time (in ns) per frame
+	 */
+	private int period = 1000000000 / GameView.FPS;
+	
+	/**
+	 * 
+	 */
+	private int noDelays;
+	
+	/**
+	 * 
+	 */
+	private final static int FPS = 60;
+	
+	/**
+	 * 
+	 */
+	private final static int NO_DELAYS_PER_YIELD = 10;
+	
 	// Constructors
 	/**
 	 * Create the view and the updater.
@@ -25,19 +51,49 @@ public class GameView extends View {
 	public GameView(MainPanel panel) {
 		super(panel);
 		this.updater = new Updater();
+		this.noDelays = 0;
 	}
 
 	// Methods
 	public void start() {
-
+		// Needed for the first loop
+		beforeTime = System.nanoTime();
 	}
 
 	public void run() {
-		try {
-			Thread.sleep(100);
-		} catch (Exception e) {
-			System.out.println("Synchronization error");
+		//this.gameUpdate(); // Game state is updated
+		//this.gameRender(); // Render to a buffer
+		// this.paintScreen(); // Draw buffer to screen
+		
+		// NanoTime after the update/render
+		afterTime = System.nanoTime();
+		
+		// Time (in ns) using to update/render
+		timeDiff = afterTime - beforeTime;
+		
+		// Time to sleep, period minus time used
+		sleepTime = (period - timeDiff) - overSleepTime;
+
+		if (sleepTime > 0) { // Some time left in this cycle
+			try {
+				// Sleep sleepTime (convert in ms)
+				Thread.sleep(sleepTime / 1000000L);
+			} catch (InterruptedException e) {
+				// If there's a sleep error
+				System.out.println("Bro error");
+				overSleepTime = (System.nanoTime() - afterTime) - sleepTime;
+			}
+		} else { // sleepTime <= 0; frame took longer than the period
+			System.out.println("Bro SUPER error");
+			
+			overSleepTime = 0L;
+
+			if (++noDelays >= GameView.NO_DELAYS_PER_YIELD) {
+				Thread.yield(); // Give another thread 	a chance to run
+				noDelays = 0;
+			}
 		}
+		beforeTime = System.nanoTime();
 	}
 
 	public void stop() {
